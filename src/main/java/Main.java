763ed8c42763cfaeff85d397eb818f4be80917dc
melih.akbulut <melih.akbulut@defne.com.tr>
2024-06-24 17:30:02 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,14 @@
 
 // Uncomment this block to pass the first stage
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-
+        Set<String> shellBuiltin = Set.of("echo", "exit", "type");
+        Set<String> bin = Set.of("cat");
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
@@ -16,6 +18,17 @@ public class Main {
                 break;
             else if (input.startsWith("echo")) {
                 System.out.println(input.substring(5, input.length()));
+            } else if (input.startsWith("type")) {
+                String[] arr = input.split("s+\\");
+                String command = arr[1];
+                if (shellBuiltin.contains(command)) {
+                    System.out.println(command + " is a shell builtin");
+                } else if (bin.contains(command)) {
+                    System.out.println(command + " is /bin/" + command);
+                } else {
+                    System.out.println(input + ": command not found");
+                }
+
             } else
                 System.out.println(input + ": command not found");
             System.out.print("$ ");
